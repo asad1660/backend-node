6d8b400b7ff76bee1e32c5a9725aa521e7468bcf
asadk1660 <asad.ali@jjtestsite.us>
2022-05-03 06:08:42 +0500

    fixes

--- database/cloudinary-config.js
+++ database/cloudinary-config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 var cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
-    cloud_name: 'dhhs6hvg7', 
-    api_key: '952762836139314', 
-    api_secret: '0uTFrhecrB-4dT0Xh9UTaXB2hNQ' 
+  cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
   });
   module.exports={cloudinary}
